05ce9aa3bbb934e8fcb4f22c5925ed932fd734ac
yamapi06150 <yamapi06150@gmail.com>
2017-07-09 22:46:33 +0800

    add scroll to function to menu in header

--- src/js/all.js
+++ src/js/all.js
@@ -8,4 +8,16 @@ $('document').ready(function () {
       $('body').removeClass('menu-show');
     }
   });
+
+  // scroll to
+  $('.menu-li-tips').on('click', function(){
+    $('html,body').animate({scrollTop:$('#tips').offset().top},800);
+  });
+  $('.menu-li-chef').on('click', function(){
+    $('html,body').animate({scrollTop:$('#chef').offset().top},800);
+  });
+  $('.menu-li-map').on('click', function(){
+    $('html,body').animate({scrollTop:$('#map').offset().top},800);
+  });
+  
 });
